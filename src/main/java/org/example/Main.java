fd87fe5283f4f267b507d221746ce1815f0f083d
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-28 14:54:54 +0200

    Create 3 instances of User

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.library.User;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        User user1 = new User("Aljoscha","Nyang");
+        User user2 = new User("Fabian", "Link");
+        User user3 = new User("Iskren", "Radev");
+
+
     }
 }
