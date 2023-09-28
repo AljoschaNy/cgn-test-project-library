5a046744b9c1f33667a6fd3aa3839817093cda18
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-28 15:43:58 +0200

    Add Novel class and refactor book instances

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 
-import org.example.library.book.Book;
 import org.example.library.User;
+import org.example.library.book.Novel;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,10 +9,12 @@ public class Main {
         User user2 = new User("Fabian", "Link");
         User user3 = new User("Iskren", "Radev");
 
-        Book book1 = new Book("Der Steppenwolf", "Hermann Hesse", "978-3-596-90078-9");
-        Book book2 = new Book("Die Verwandlung", "Franz Kafka", "978-3-596-90400-8");
-        Book book3 = new Book("Im Westen nichts Neues", "Erich Maria Remarque", "978-3-499-22755-5");
-        Book book4 = new Book("Der Vorleser", "Bernhard Schlink", "978-3-442-31056-7");
-        Book book5 = new Book("Das Parfum", "Patrick Süskind", "978-3-257-23028-4");
+        Novel book1 = new Novel("Der Steppenwolf", "Hermann Hesse", "978-3-596-90078-9","Roman, Philosophie" );
+        Novel book2 = new Novel("Die Verwandlung", "Franz Kafka", "978-3-596-90400-8", "Novelle, Existenzialismus");
+        Novel book3 = new Novel("Im Westen nichts Neues", "Erich Maria Remarque", "978-3-499-22755-5", "Kriegsroman");
+        Novel book4 = new Novel("Der Vorleser", "Bernhard Schlink", "978-3-442-31056-7", "Zeitgeschichtlicher Roman");
+        Novel book5 = new Novel("Das Parfum", "Patrick Süskind", "978-3-257-23028-4", "Historischer Roman, Kriminalroman");
+
+        System.out.println(book1);
     }
 }
---
+++ src/main/java/org/example/library/book/Novel.java
@@ -0,0 +1,46 @@
+package org.example.library.book;
+
+import org.example.library.book.Book;
+
+import java.util.Objects;
+
+public class Novel extends Book {
+    private String genre;
+    public Novel(String title, String author, String isbn, String genre) {
+        super(title, author, isbn);
+        this.genre = genre;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Novel novel = (Novel) o;
+        return Objects.equals(genre, novel.genre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), genre);
+    }
+
+    @Override
+    public String toString() {
+        return "Novel{" +
+                "title='" + super.getTitle() + '\'' +
+                ", author='" + super.getAuthor() + '\'' +
+                ", isbn='" + super.getIsbn() + '\'' +
+                ", isBorrowed=" + super.getBorrowedStatus() +
+                "genre='" + genre + '\'' +
+                '}';
+    }
+}
